c9998d7e18ef9acaff122cb14e3308c99d3d8257
Luis Ferreira <luis19nn@gmail.com>
2022-01-23 13:48:40 -0300

    Scroll the background

--- index.js
+++ index.js
@@ -41,7 +41,32 @@ class Player {
     }
 }
 
+class Platform {
+    constructor({ x, y }) {
+        this.position = {
+            x,
+            y,
+        };
+        this.width = 200;
+        this.height = 20;
+    }
+
+    draw() {
+        context.fillStyle = "blue";
+        context.fillRect(
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        );
+    }
+}
+
 const player = new Player();
+const platforms = [
+    new Platform({ x: 400, y: 400 }),
+    new Platform({ x: 800, y: 420 }),
+];
 
 const keys = {
     right: {
@@ -55,15 +80,43 @@ const keys = {
 function animate() {
     requestAnimationFrame(animate);
     context.clearRect(0, 0, canvas.width, canvas.height);
-    player.update();
 
-    if (keys.right.pressed) {
+    player.update();
+    platforms.forEach((platform) => {
+        platform.draw();
+    });
+
+    //player movement
+    if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = 5;
-    } else if (keys.left.pressed) {
+    } else if (keys.left.pressed && player.position.x > 100) {
         player.velocity.x = -5;
     } else {
         player.velocity.x = 0;
+
+        if (keys.right.pressed) {
+            platforms.forEach((platform) => {
+                platform.position.x -= 5;
+            });
+        } else if (keys.left.pressed) {
+            platforms.forEach((platform) => {
+                platform.position.x += 5;
+            });
+        }
     }
+
+    //collision between player and platform
+    platforms.forEach((platform) => {
+        if (
+            player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >=
+                platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width
+        ) {
+            player.velocity.y = 0;
+        }
+    });
 }
 animate();
 
