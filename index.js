0a61406f08c13629c1aeb9033ca05eff811cd6ca
Luis Ferreira <luis19nn@gmail.com>
2022-01-23 12:04:45 -0300

    Player Creation

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-console.log(context);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+    constructor() {
+        this.position = {
+            X: 100,
+            Y: 100,
+        };
+        this.width = 30;
+        this.height = 30;
+    }
+
+    draw() {
+        context.fillStyle = "red";
+        context.fillRect(
+            this.position.X,
+            this.position.Y,
+            this.width,
+            this.height
+        );
+    }
+}
+
+const player = new Player();
+player.draw();
