9a08aa3f558c3544ab8b46ad69d3c1822bf4cc9b
Luis Ferreira <luis19nn@gmail.com>
2022-01-23 13:08:47 -0300

    Player Movements

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ class Player {
 
     update() {
         this.draw();
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
@@ -42,9 +43,66 @@ class Player {
 
 const player = new Player();
 
+const keys = {
+    right: {
+        pressed: false,
+    },
+    left: {
+        pressed: false,
+    },
+};
+
 function animate() {
     requestAnimationFrame(animate);
     context.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5;
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5;
+    } else {
+        player.velocity.x = 0;
+    }
 }
 animate();
+
+window.addEventListener("keydown", ({ keyCode }) => {
+    switch (keyCode) {
+        //a
+        case 65:
+            keys.left.pressed = true;
+            break;
+        //w
+        case 87:
+            player.velocity.y -= 20;
+            break;
+        //d
+        case 68:
+            keys.right.pressed = true;
+            break;
+        //s
+        case 83:
+            break;
+    }
+});
+
+window.addEventListener("keyup", ({ keyCode }) => {
+    switch (keyCode) {
+        //a
+        case 65:
+            keys.left.pressed = false;
+            break;
+        //w
+        case 87:
+            player.velocity.y = 0;
+            break;
+        //d
+        case 68:
+            keys.right.pressed = false;
+            break;
+        //s
+        case 83:
+            break;
+    }
+});
